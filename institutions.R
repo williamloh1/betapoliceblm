a1d171fa5025362d08c59f313c0bded0a4d6c588
lingechun <60479131+lingechun@users.noreply.github.com>
2020-02-27 11:20:40 -0600

    Update institutions.R

--- institutions.R
+++ institutions.R
@@ -4,6 +4,7 @@ VSG.inst <-VSG %>%
 for(i in seq_along(VSG.inst)){
   VSG.inst[,i]= na_if(VSG.inst[[i]],VSG.inst[[i]]>4)
 }
+
 mean.inst <- map(VSG.inst, mean, na.rm = TRUE)
 median.inst <-map(VSG.inst, median, na.rm = TRUE)
 mean.inst <- sort(mean.inst[[]], decreasing = TRUE)
@@ -13,3 +14,20 @@ mean.plot <- plot(1:length(mean.inst),mean.inst,pch=18, col="blue",xlab="Institu
 mean.plot <- text(1:length(mean.inst),mean.inst,names(mean.inst),cex=0.5, col="red")
 median.plot <- plot(1:length(median.inst),median.inst,pch=18, col="blue",xlab="Institutions",ylab="Median Trust")
 median.plot <- text(1:length(median.inst),median.inst,names(median.inst),cex=0.5, col="red")
+
+my.fun <- function(x, na){
+  x[x>na]<-NA
+  return(x)
+}
+
+
+VSG.inst2 <- apply(VSG.inst, 2, my.fun, na = 4)
+mean <- apply(VSG.inst2, 2, mean, na.rm=T )
+mean <- sort(mean, decreasing = F)
+?mean
+median <- apply(VSG.inst2, 2, median, na.rm=T )
+mean.plot <- plot(1:length(mean),mean,pch=18, col="blue",xlab="Institutions",ylab="Mean Trust")
+mean.plot <- text(1:length(mean),mean,names(mean),cex=0.5, col="red")
+median.plot <- plot(1:length(median.inst),median.inst,pch=18, col="blue",xlab="Institutions",ylab="Median Trust")
+median.plot <- text(1:length(median.inst),median.inst,names(median.inst),cex=0.5, col="red")
+
