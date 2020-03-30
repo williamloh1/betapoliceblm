b26a7d44f8685f474c7e6539d497324ccfcf0fb9
lingechun <60479131+lingechun@users.noreply.github.com>
2020-03-30 13:11:49 -0500

    Update 3-27 class exercise.R

--- 3-27 class exercise.R
+++ 3-27 class exercise.R
@@ -1,5 +1,9 @@
 senateData<-read.csv("http://politicaldatascience.com/PDS/Datasets/SenateForecast/CandidateLevel.csv")
 candidates2018 <- read.csv("http://politicaldatascience.com/PDS/Datasets/SenateForecast/CandidateLevel2018.csv")
+library(tidyverse)
+candidates2018 <- candidates2018 %>%
+  rename(weightexperience=Weightedexperience) %>%
+  rename(PercentageRaised=Percentage.Raised)
 
 #split data
 library(rsample)
@@ -16,5 +20,11 @@ ComplexModelTrain <- lm(VotePercentage~pvi*Republican+weightexperience+Incumbent
 summary(ComplexModelTrain)
 ComplexModelPredictions<-predict(ComplexModelTrain, newdata=senate_test)
 sqrt(mean((ComplexModelPredictions-senate_test$VotePercentage)^2))
-
-
+#predict 2018 candidates
+vote <- predict(ComplexModelTrain, 
+        newdata=candidates2018[which(candidates2018$Candidateidentifier %in% c("2018MOMcCaskill", "2018OHBrown", "2018WVManchin")), ])
+vote <- as.data.frame(vote)
+rownames(vote)[1] <- "2018OHBrown"
+rownames(vote)[2] <- "2018MOMcCaskill"
+rownames(vote)[3] <- "2018WVManchin"
+vote
