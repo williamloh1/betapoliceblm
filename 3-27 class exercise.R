3270c3d45d2f265c45579bae1563c2d2f36d28db
lingechun <60479131+lingechun@users.noreply.github.com>
2020-03-30 12:36:25 -0500

    Update 3-27 class exercise.R

--- 3-27 class exercise.R
+++ 3-27 class exercise.R
@@ -1,4 +1,5 @@
 senateData<-read.csv("http://politicaldatascience.com/PDS/Datasets/SenateForecast/CandidateLevel.csv")
+candidates2018 <- read.csv("http://politicaldatascience.com/PDS/Datasets/SenateForecast/CandidateLevel2018.csv")
 #split data
 library(rsample)
 set.seed(10000)
@@ -13,4 +14,5 @@ sqrt(mean((SimpleModelPredictions-senate_train$VotePercentage)^2))
 ComplexModelTrain <- lm(VotePercentage~pvi+weightexperience+GenericBallotSept+Incumbent+PercentageRaised, data=senate_train)
 ComplexModelPredictions<-predict(ComplexModelTrain, newdata=senate_test)
 sqrt(mean((ComplexModelPredictions-senate_train$VotePercentage)^2))
+
 
