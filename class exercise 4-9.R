9f74edf8e9eaab66135646cbb237685a70bbb44a
Alper Sukru Gencer <alpergencer@wust.edu>
2020-04-08 16:36:00 -0500

    Update class exercise 4-9.R

--- class exercise 4-9.R
+++ class exercise 4-9.R
@@ -22,6 +22,20 @@ lm(fatal_rate ~ beertax, data=AZ)$coefficients
 lm(fatal_rate ~ beertax, data=AR)$coefficients
 
 
+########  Put into slack what you think explains the flip in sign. 
+####        (You don't need to put up the lines, but drawing them may help you think about it.)
+
+model2<-lm(fatal_rate ~ beertax + factor(state), data=Fatalities)
+head(model2$coefficients)
+
+#---  It is probably because of the unobserved state-fixed factors, such
+#---    as driving regulations, drinking culture, or other socio-economic
+#---    factors, such as median income, education, unemployment etc. Another
+#---    reasonable explanation is that the states with grater drinking habits
+#---    are likely to have higher fatality rates, which make these states 
+#---    more restrictive about tax regulations. Recall the hospitalization-
+#---    average health comparison. Beer tax is likely to be assigned states
+#---    with much more drinking habit.
 
 #group assignment
 palestinians<- read.csv("https://jmontgomery.github.io/ProblemSets/longo.csv")
