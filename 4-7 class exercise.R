e7794bc2474202a7710b5192d0d33b9f41ddb184
William Loh <william.loh@wustl.edu>
2020-04-08 16:19:22 -0500

    Update 4-7 class exercise.R

--- 4-7 class exercise.R
+++ 4-7 class exercise.R
@@ -1,67 +1,15 @@
-rm(list = ls())
-
-#Per slide 35
-resume<-read.csv("http://politicaldatascience.com/PDS/Datasets/resume.csv")
-summary(lm(call~race, data=resume))
-low.race.coef <- 0.032033-qt(0.975, 4868)*0.007785
-high.race.coef <- 0.032033+qt(0.975, 4868)*0.007785
-paste("The 95% CI for the race treatment is between", low.race.coef, "and", high.race.coef)
-
-bootstrapped.reserved <- list()
-for (i in 1:1000){
-  resume.sample <- resume[sample (1:nrow(resume), nrow(resume), replace=T),]
-  bootstrapped.reserved[[i]] <- coef(lm (call~race, data=resume.sample))[2]
-}
-mean(unlist(bootstrapped.reserved))
-sd(unlist(bootstrapped.reserved))
-low.boot.reserved.coef <- mean(unlist(bootstrapped.reserved))-qt(0.975, 1000)*sd(unlist(bootstrapped.reserved))
-high.boot.reserved.coef <-mean(unlist(bootstrapped.reserved))+qt(0.975, 1000)*sd(unlist(bootstrapped.reserved))
-paste("The 95% CI for the bootstrapped race treatment is between", low.boot.reserved.coef, "and", high.boot.reserved.coef)
-
-
-
-#Group assignment
-women <- read.csv("https://raw.githubusercontent.com/kosukeimai/qss/master/PREDICTION/women.csv")
-model <- lm(water~reserved, data=women)
-summary(model)
-low.reserved.coef <- 9.252-qt(0.975, 320)*3.948
-high.reserved.coef <- 9.252+qt(0.975, 320)*3.948
-paste("The 95% CI for the reserved treatment is between", low.reserved.coef, "and", high.reserved.coef)
-
-#using bootstrapped standard errors
-set.seed(999)
-bootstrapped.reserved <- list()
-for (i in 1:1000){
-  women.sample <- women[sample (1:nrow(women), nrow(women), replace=T),]
-  bootstrapped.reserved[[i]] <- coef(lm (water~reserved, data=women.sample))[2]
-}
-mean(unlist(bootstrapped.reserved))
-sd(unlist(bootstrapped.reserved))
-low.boot.reserved.coef <- mean(unlist(bootstrapped.reserved))-qt(0.975, 1000)*sd(unlist(bootstrapped.reserved))
-high.boot.reserved.coef <-mean(unlist(bootstrapped.reserved))+qt(0.975, 1000)*sd(unlist(bootstrapped.reserved))
-paste("The 95% CI for the bootstrapeed reserved treatment is between", low.boot.reserved.coef, "and", high.boot.reserved.coef)
-
-
-
-########  With controls
-
-
-model.2 <- lm(water ~ reserved + irrigation, data=women)
-summary(model.2)
-low.reserved.coef.2 <- model.2$coefficients["reserved"] - qnorm(0.975)*sqrt(vcov(model.2)["reserved","reserved"])
-high.reserved.coef.2 <- model.2$coefficients["reserved"] + qnorm(0.975)*sqrt(vcov(model.2)["reserved","reserved"])
-paste("The 95% CI for the controlled reserved treatment is between", low.reserved.coef.2, "and", high.reserved.coef.2)
-
-#using bootstrapped standard errors
-set.seed(999)
-bootstrapped.reserved.2 <- list()
-for (i in 1:1000){
-  women.sample <- women[sample (1:nrow(women), nrow(women), replace=T),]
-  bootstrapped.reserved.2[[i]] <- coef(lm (water~reserved+irrigation, data=women.sample))[2]
-}
-mean(unlist(bootstrapped.reserved.2))
-sd(unlist(bootstrapped.reserved.2))
-low.boot.reserved.coef.2 <- mean(unlist(bootstrapped.reserved.2))-qt(0.975, 1000)*sd(unlist(bootstrapped.reserved.2))
-high.boot.reserved.coef.2 <-mean(unlist(bootstrapped.reserved.2))+qt(0.975, 1000)*sd(unlist(bootstrapped.reserved.2))
-paste("The 95% CI for the bootstrapeed reserved treatment is between", low.boot.reserved.coef.2, "and", high.boot.reserved.coef.2)
-
+df<- read.csv("https://jmontgomery.github.io/ProblemSets/longo.csv")
+df2008 <- subset(df,df$Sample2009=="0")
+df2008ZA <- subset(df2008,df2008$ZA=="1")
+df2008WN <- subset(df2008,df2008$ZA=="0")
+df2009 <- subset(df,df$Sample2009=="1")
+df2009ZA <- subset(df2009,df2009$ZA=="1")
+df2009WN <- subset(df2009,df2009$ZA=="0")
+mil2008ZA <- mean(df2008ZA$militancy,na.rm=T)
+mil2008WN <- mean(df2008WN$militancy,na.rm=T)
+mil2009ZA <- mean(df2009ZA$militancy,na.rm=T)
+mil2009WN <- mean(df2009WN$militancy,na.rm=T)
+diff <- c("Mil2008ZA"=mil2008ZA,"Mil2008WN"=mil2008WN,"Mil2009ZA"=mil2009ZA,"Mil2009WN"=mil2009WN)
+diff 
+diffindiff <- mil2009ZA-((mil2009WN-mil2008WN)+mil2008ZA)
+diffindiff
