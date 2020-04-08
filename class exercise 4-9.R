7095f566b8efd817273955f09c49e627250a1aff
lingechun <60479131+lingechun@users.noreply.github.com>
2020-04-08 09:46:04 -0500

    Create class exercise 4-9.R

---
+++ class exercise 4-9.R
@@ -0,0 +1,31 @@
+#Per slide 11
+congress <- read.csv("https://jmontgomery.github.io/ProblemSets/incumbents.csv")
+lm(voteshare~incspend, data=congress)
+lm(voteshare~incspend+chalspend+presvote+chalquality, data=congress)
+library(tidyverse)
+congress %>% group_by(incspend>mean(incspend, na.rm=T)) %>%
+  summarize(Chalspend = mean(chalspend, na.rm=TRUE), 
+            Presvote=mean(presvote, na.rm=TRUE),
+            Chalquality = mean(chalquality, na.rm=TRUE))
+
+#Per slide 21
+library(AER)
+data("Fatalities")
+Fatalities$fatal_rate <- Fatalities$fatal / Fatalities$pop * 10000
+summary(lm(fatal_rate ~ beertax, data=Fatalities))
+
+AL <- Fatalities[Fatalities$state=="al", ]
+AZ <- Fatalities[Fatalities$state=="az", ]
+AR <- Fatalities[Fatalities$state=="ar", ]
+lm(fatal_rate ~ beertax, data=AL)$coefficients
+lm(fatal_rate ~ beertax, data=AZ)$coefficients
+lm(fatal_rate ~ beertax, data=AR)$coefficients
+
+
+
+#group assignment
+palestinians<- read.csv("https://jmontgomery.github.io/ProblemSets/longo.csv")
+my.model <- lm(militancy~ZA+Sample2009+Sample2009*ZA, data = palestinians)
+library(sjPlot)
+plot_model(my.model, type="int")
+summary(my.model)
